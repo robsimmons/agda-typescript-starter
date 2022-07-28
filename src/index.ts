e19dfd9d5deaba4af4a2646afefbd232b73f4a57
Rob Simmons <robsimmons@gmail.com>
2022-07-28 14:11:58 -0400

    More cruft

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-//import { Tree, Nat, height } from 'jAgda.Tree';
-//console.log(Tree.leaf);
-//console.log(Tree.node(Tree.leaf)(Tree.leaf));
+import { Tree, Nat, height } from 'jAgda.Tree';
+console.log(Tree.leaf);
+console.log(Tree.node(Tree.leaf)(Tree.leaf));
 console.log('Hello world!');
 
