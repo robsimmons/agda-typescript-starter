6f1d41de1e1f2de33cc98c84d5b047547f596afa
Rob Simmons <robsimmons@gmail.com>
2022-07-29 06:52:54 -0400

    Undo changes to test file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Tree, height } from 'jAgda.Tree';
+import { Tree, height } from '../agda/build/jAgda.Tree';
 console.log(Tree.leaf);
 console.log(Tree.node(Tree.leaf)(Tree.leaf));
 console.log(height(Tree.leaf));
